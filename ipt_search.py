8affdf74c7d7cbceb5b89701456e3d111b2d7782
han9wo0 <115068605+lokiju12@users.noreply.github.com>
2023-07-06 17:12:15 +0900

    Add files via upload

--- ipt_search.py
+++ ipt_search.py
@@ -12,10 +12,15 @@ def download_csv(url, save_path):
     else:
         print("\n파일을 다운로드할 수 없습니다.\n")
 
-url = "http://<주소입력>/ipt/DBTable/user_profile.csv"
+url = "http://10.10.10.20/ipt/DBTable/user_profile.csv"
 save_path = "c:/domain/ipt/user_profile.csv"
 
 download_csv(url, save_path)
+
+
+
+
+
 
 import pandas as pd
 
@@ -23,13 +28,19 @@ def search_phone_number(csv_path, search_value):
     df = pd.read_csv(csv_path, encoding='ANSI')
     columns_to_exclude = ['v_port', 'MAC addr', 'mode', 'attached UC phone', 'Detail Type']
     search_result = df[df['Phone Number'].astype(str).str.contains(str(search_value))].drop(columns=columns_to_exclude)
+    search_result = search_result.sort_values(by='Phone Number', ascending=True)
     return search_result
 
 
 def search_ip(csv_path, search_value):
     df = pd.read_csv(csv_path, encoding='ANSI')
     columns_to_exclude = ['v_port', 'MAC addr', 'mode', 'attached UC phone', 'Detail Type']
-    search_result = df[df['IP address'].astype(str).str.contains(str(search_value))].drop(columns=columns_to_exclude)
+    search_result = df[df['IP address'].astype(str).str.startswith(str(search_value))].drop(columns=columns_to_exclude)
+    # IP 주소를 숫자로 변환하여 정렬
+    search_result['IP address'] = search_result['IP address'].apply(lambda x: sum([256**i*int(j) for i, j in enumerate(x.split('.')[::-1])]))
+    search_result = search_result.sort_values(by='IP address', ascending=True)
+    # 다시 IP 주소 형식으로 변환
+    search_result['IP address'] = search_result['IP address'].apply(lambda x: '.'.join([str((x >> i) % 256) for i in (24, 16, 8, 0)]))
     return search_result
 
 
@@ -38,13 +49,18 @@ def search_directory(csv_path, search_value):
     columns_to_exclude = ['v_port', 'MAC addr', 'mode', 'attached UC phone', 'Detail Type']
     search_result = df[df['Directory'].astype(str).str.contains(str(search_value))]
     search_result = search_result.drop(columns=columns_to_exclude)
+    search_result = search_result.sort_values(by='Directory', ascending=True)
     return search_result
+
 
 csv_path = "c:/domain/ipt/user_profile.csv"
 df = pd.read_csv(csv_path, encoding='ANSI')
 
+
 # # 화면 지우고 프로그램 시작하기
 # os.system('cls' if os.name == 'nt' else 'clear')
+
+
 
 while True:
     
@@ -80,6 +96,7 @@ while True:
         
         input("\n계속하려면 엔터 키를 눌러주세요.\n")
 
+
     elif option == "2":
         os.system('cls' if os.name == 'nt' else 'clear')
         search_value = input("\nIP Address : ")
@@ -92,6 +109,7 @@ while True:
         
         input("\n계속하려면 엔터 키를 눌러주세요.\n")
 
+
     elif option == "3":
         os.system('cls' if os.name == 'nt' else 'clear')
         search_value = input("\n디렉토리 : ")
@@ -103,6 +121,8 @@ while True:
             print("\n검색 결과가 없습니다.\n")
         
         input("\n계속하려면 엔터 키를 눌러주세요.\n")
+
+
 
     elif option == "9":
         print("\n프로그램을 종료합니다.\n")
